c02c75fe89f400a159db1b160457062e951308fb
Jain <ayush.jain01@sap.com>
2024-09-15 16:26:04 +0530

    Completed tutorials for Query Parameters section of Tutorial - User Guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 class ModelName(str, Enum):
@@ -10,15 +11,30 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/items/")
+async def get_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if short is True:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get("/users/me")
@@ -54,3 +70,15 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
