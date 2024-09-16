de8a7d0d6f3f887d20a65493dc31cc7db08d9042
Jain <ayush.jain01@sap.com>
2024-09-16 21:47:56 +0530

    Completed request body tutorials

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     restnet = "restnet"
@@ -82,3 +89,17 @@ async def read_user_item(user_id: int, item_id: str, q: str, short: bool = False
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
